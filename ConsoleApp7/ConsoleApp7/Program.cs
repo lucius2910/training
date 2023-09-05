0c98d1c4944802f1cf539e3beecb12d44326435b
ngoc <maiducngoctn@gmail.com>
2023-09-05 20:08:03 +0700

    oke

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,18 @@
+﻿        int chieudai = 6; 
+        for (int i = 1; i <= chieudai; i++)
+        {
+       
+            for (int j = 1; j <= chieudai - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 1; k <= 2 * i - 1; k++)
+            {
+                Console.Write("*");
+            }
+
+          
+            Console.WriteLine();
+        }
+    
