144381d4b75e605f6301ee46b188dbe619b6fa52
thinlc <luucongthin@gmail.com>
2023-09-05 20:37:54 +0700

    fix BT 2

--- ConsoleApp7/ConsoleApp7/Program.cs
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -1,18 +1,39 @@
-﻿        int chieudai = 6; 
-        for (int i = 1; i <= chieudai; i++)
-        {
+﻿int chieudai = 6; 
+for (int i = 1; i <= chieudai; i++)
+{
        
-            for (int j = 1; j <= chieudai - i; j++)
-            {
-                Console.Write(" ");
-            }
+    for (int j = 1; j <= chieudai - i; j++)
+    {
+        Console.Write("0 ");
+    }
 
-            for (int k = 1; k <= 2 * i - 1; k++)
-            {
-                Console.Write("*");
-            }
+    for (int k = 1; k <= 2 * i - 1; k++)
+    {
+        Console.Write("* ");
+    }
 
-          
-            Console.WriteLine();
-        }
-    
+    for (int j = chieudai * 2 - 1; j >= chieudai; j--)
+    {
+        if (j - chieudai >= i)
+            Console.Write("0 ");
+    }
+ 
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("=====================================================");
+
+for (int i = 1; i <= chieudai; i++)
+{
+
+    for (int k = 1; k <= (chieudai * 2 - 1); k++)
+    {
+        if (k >= i && k <= (chieudai * 2 - i))
+            Console.Write("* ");
+        else
+            Console.Write("0 ");
+    }
+
+    Console.WriteLine();
+}
