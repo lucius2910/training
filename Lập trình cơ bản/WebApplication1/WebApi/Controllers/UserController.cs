f77c2ee9e760a94969df44f7d4e0482aa2bdca92
ngoc <maiducngoctn@gmail.com>
2023-10-17 21:43:13 +0700

    oke

---
+++ Lập trình cơ bản/WebApplication1/WebApi/Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Entires;
+using Services.Entities;
+using Services.Interfaces;
+
+namespace WebApi.Controllers
+{
+	[Route("api/users/")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private IUserServices _userService { get; set; }
+
+		public UserController(IUserServices userService)
+		{
+			_userService = userService;
+		}
+
+		[Route("")]
+		[HttpGet]
+		public IActionResult Get()
+		{
+			var users = _userService.GetList();
+			if (users.Count > 0)
+				return Ok(users);
+			else
+				return BadRequest(users);
+		}
+
+		[Route("add-new")]
+		[HttpPost]
+		public async Task<IActionResult> AddNew([FromBody] User item)
+		{
+			var rowFetched = await _userService.Add(item);
+			if (rowFetched > 0)
+				return Ok(new { message = "add success" });
+			else
+				return BadRequest(new { message = "add fail" });
+		}
+
+
+		[Route("delete/{id}")]
+		[HttpDelete]
+		public async Task<IActionResult> Delete([FromRoute] int id)
+		{
+			var rowFetched = await _userService.Delete(id);
+			if (rowFetched > 0)
+				return Ok(new { message = "delete success" });
+			else
+				return BadRequest(new { message = "delete fail" });
+
+		}
+
+		[Route("update/{id}")]
+		[HttpPut]
+
+		public async Task<IActionResult> Update([FromRoute] int id, [FromBody] User item)
+		{
+			//  Update
+			var rowFetched = await _userService.Update(id, item);
+			if (rowFetched > 0)
+				return Ok(new { message = "Update success" });
+			else
+				return BadRequest(new { message = "Update fail" });
+		}
+
+	}
+}
---
+++ Lập trình cơ bản/WebApplication1/WebApi/Controllers/VehicleController.cs
@@ -0,0 +1,53 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Services.Entities;
+using Services.Interfaces;
+
+namespace WebApi.Controllers
+{
+	[Route("api/vehicle/")]
+	[ApiController]
+	public class VehicleController : ControllerBase
+	{
+		private IVehicleServices _vehicleService { get; set; }
+
+		public VehicleController(IVehicleServices vehicleService)
+		{
+			_vehicleService = vehicleService;
+		}
+
+		[Route("")]
+		[HttpGet]
+		public IActionResult Get()
+		{
+			var vehicles = _vehicleService.GetList();
+			if (vehicles.Count > 0)
+				return Ok(vehicles);
+			else
+				return BadRequest(vehicles);
+		}
+
+		[Route("add-new")]
+		[HttpPut]
+		public async Task<IActionResult> AddNew([FromBody] Vehicle item)
+		{
+			var rowFetched = await _vehicleService.Add(item);
+			if (rowFetched > 0)
+				return Ok(new { message = "add success" });
+			else
+				return BadRequest(new { message = "add fail" });
+		}
+
+
+		[Route("delete/{id}")]
+		[HttpDelete]
+		public async Task<IActionResult> Delete([FromRoute] int id)
+		{
+			var rowFetched = await _vehicleService.Delete(id);
+			if (rowFetched > 0)
+				return Ok(new { message = "delete success" });
+			else
+				return BadRequest(new { message = "delete fail" });
+		}
+	}
+}
